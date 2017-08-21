2be71253de3e727cabb4efaa67432aec25ffcfb2
leshayface <leshaywebdzn@gmail.com>
2017-08-21 17:31:48 +0200

    app

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const SingleGrid = React.createClass({
+  render () {
+    return (
+      <div className="single-photo">
+        Im the singlephoto
+      </div>
+    )
+  }
+});
+
+export default SingleGrid;
